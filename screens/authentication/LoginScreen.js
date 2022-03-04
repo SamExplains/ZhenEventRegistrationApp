e3400716b4c1f16a5b00561ef2105d9585b5b292
your_name <your_email_id>
2022-03-03 17:21:06 -0800

    Updated the Login and Signup wth the redesign.

--- screens/authentication/LoginScreen.js
+++ screens/authentication/LoginScreen.js
@@ -16,6 +16,7 @@ import {
 } from "@ui-kitten/components";
 import { GoogleIcon } from "../../assets/icons";
 import SignupScreen from "./SignupScreen";
+import PasswordResetScreen from "./PasswordResetScreen";
 import React, { Component, useState } from "react";
 
 const email = null;
@@ -70,94 +71,103 @@ const checked = false;
 //     );
 //   }
 // }
+const onForgotPassword = () => {
+  alert("onForgotPassword");
+};
 
 export const LoginScreen = () => {
   // Temporarily toggle to switch between the Login view and the Google/Signup view
-  const [signup, setSignup] = useState(false);
+  const [signup, setSignup] = useState(2);
+  const [forgotPassword] = useState(false);
 
   const onToggleSignup = () => {
-    setSignup(!signup);
+    // Toggle based on what screen to show
+    setSignup(3);
   };
+
+  const show = () => {
+    if (signup === 1) {
+      return (
+        <Layout>
+          <Layout style={styles.backgroundImageContainer}>
+            <Image
+              style={styles.backgroundImage}
+              source={require("../../assets/Group1.png")}
+            />
+          </Layout>
+
+          <Text
+            category="c2"
+            style={{ paddingLeft: 10, marginTop: 20, color: "#301A4B" }}
+          >
+            WELCOME TO
+          </Text>
+          <Text
+            category="h6"
+            style={{
+              fontWeight: "bold",
+              paddingLeft: 10,
+              color: "#301A4B",
+              marginBottom: 20,
+            }}
+          >
+            Event Registration
+          </Text>
+          <Input
+            style={styles.input}
+            value={email}
+            label="E-mail"
+            placeholder="Enter your email addess"
+          />
+          <Input
+            style={styles.input}
+            value={password}
+            label="Password"
+            placeholder="Enter your password"
+          />
+          <Text
+            style={{
+              textAlign: "right",
+              marginRight: 15,
+              marginTop: 10,
+              marginBottom: 40,
+              color: "#0B2F8D",
+              fontWeight: "bold",
+            }}
+            onPress={onForgotPassword}
+          >
+            Forgot Password?
+          </Text>
+
+          <Button style={styles.button} size="medium">
+            Let's Go!
+          </Button>
+          <Button size="medium" appearance="outline" style={styles.buttonAlt}>
+            <Text style={{ fontWeight: "700", color: "#454545" }}>
+              Log in with Google
+            </Text>
+          </Button>
+
+          <Text style={styles.noaccount}>
+            Don't have an account?
+            <Text style={styles.signUp} onPress={onToggleSignup}>
+              {" "}
+              Sign Up
+            </Text>
+          </Text>
+        </Layout>
+      );
+    } else if (signup === 2) {
+      return <SignupScreen />;
+    } else {
+      return <PasswordResetScreen />;
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.scrollView}>
-        <Layout style={styles.container}>
-          {!signup ? (
-            <Layout>
-              <Layout style={styles.backgroundImageContainer}>
-                <Image
-                  style={styles.backgroundImage}
-                  source={require("../../assets/Group1.png")}
-                />
-              </Layout>
-
-              <Text
-                category="c2"
-                style={{ paddingLeft: 10, marginTop: 20, color: "#301A4B" }}
-              >
-                WELCOME TO
-              </Text>
-              <Text
-                category="h6"
-                style={{
-                  fontWeight: "bold",
-                  paddingLeft: 10,
-                  color: "#301A4B",
-                  marginBottom: 20,
-                }}
-              >
-                Event Registration
-              </Text>
-              <Input
-                style={styles.input}
-                value={email}
-                label="E-mail"
-                placeholder="Enter your email addess"
-              />
-              <Input
-                style={styles.input}
-                value={password}
-                label="Password"
-                placeholder="Enter your password"
-              />
-              <Text
-                style={{
-                  textAlign: "right",
-                  marginRight: 15,
-                  marginTop: 10,
-                  marginBottom: 40,
-                  color: "#0B2F8D",
-                  fontWeight: "bold",
-                }}
-              >
-                Forgot Password?
-              </Text>
-
-              <Button style={styles.button} size="medium">
-                Let's Go!
-              </Button>
-              <Button
-                size="medium"
-                appearance="outline"
-                style={styles.buttonAlt}
-              >
-                <Text style={{ fontWeight: "700", color: "#454545" }}>
-                  Log in with Google
-                </Text>
-              </Button>
-
-              <Text style={styles.noaccount}>
-                Don't have an account
-                <Text style={styles.signUp} onPress={onToggleSignup}>
-                  {" "}
-                  Sign Up
-                </Text>
-              </Text>
-            </Layout>
-          ) : (
-            <SignupScreen />
-          )}
-        </Layout>
+        <Layout style={styles.container}>{show()}</Layout>
       </ScrollView>
     </SafeAreaView>
   );
@@ -198,10 +208,12 @@ const styles = StyleSheet.create({
   },
   noaccount: {
     textAlign: "center",
+    marginTop: 10,
   },
   signUp: {
     fontWeight: "bold",
     color: "#0B2F8D",
+    marginTop: 10,
   },
   backgroundImageContainer: {
     backgroundColor: "#F7F0FF",
--- screens/authentication/SignupScreen.js
+++ screens/authentication/SignupScreen.js
@@ -12,6 +12,7 @@ import {
   Button,
   Layout,
   CheckBox,
+  StyleService,
 } from "@ui-kitten/components";
 import React, { Component, useState } from "react";
 
@@ -22,55 +23,89 @@ export const SignupScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.scrollView}>
-        <Layout style={styles.container}>
-          <Input
-            style={styles.input}
-            value={email}
-            label="E-mail"
-            placeholder="Enter your email addess"
-          />
-          <Input
-            style={styles.input}
-            value={email}
-            label="ZIP"
-            placeholder="00000"
-          />
-          <Input
-            style={styles.input}
-            value={email}
-            label="Name"
-            placeholder="John Appleseed"
-          />
-          <Input
-            style={styles.input}
-            value={email}
-            label="Password"
-            placeholder=""
-          />
-          <Input
-            style={styles.input}
-            value={email}
-            label="Confirm Password"
-            placeholder=""
-          />
-          <Input
-            style={styles.input}
-            value={email}
-            label="Phone"
-            placeholder="000-0000"
-          />
+        <Layout>
+          <Layout style={styles.hero}>
+            <Text
+              category="c2"
+              style={{ paddingLeft: 15, marginTop: 50, color: "white" }}
+            >
+              WELCOME TO
+            </Text>
+            <Text
+              category="h6"
+              style={{
+                fontWeight: "bold",
+                paddingLeft: 15,
+                color: "white",
+              }}
+            >
+              Event Registration
+            </Text>
+          </Layout>
+          <Layout style={styles.container}>
+            <Text
+              category="h6"
+              style={{
+                fontWeight: "bold",
+                paddingLeft: 15,
+                marginTop: 25,
+                marginBottom: 25,
+                color: "#301A4B",
+              }}
+            >
+              Sign Up
+            </Text>
+            <Input
+              style={styles.input}
+              value={email}
+              label="E-mail"
+              placeholder="Enter your email addess"
+            />
+            <Input
+              style={styles.input}
+              value={email}
+              label="Zip Code"
+              placeholder="00000"
+            />
+            <Input
+              style={styles.input}
+              value={email}
+              label="Name"
+              placeholder="John Appleseed"
+            />
+            <Input
+              style={styles.input}
+              value={email}
+              label="Password (at least 8 characters)"
+              placeholder=""
+            />
+            <Input
+              style={styles.input}
+              value={email}
+              label="Confirm Password"
+              placeholder=""
+            />
+            <Input
+              style={styles.input}
+              value={email}
+              label="Phone Number (Optional)"
+              placeholder="000-0000"
+            />
 
-          <CheckBox
-            style={styles.checkBox}
-            checked={checked}
-            onChange={(nextChecked) => setChecked(nextChecked)}
-          >
-            Terms of Conditions and Privacy Policy
-          </CheckBox>
+            <CheckBox
+              style={styles.checkBox}
+              checked={checked}
+              onChange={(nextChecked) => setChecked(nextChecked)}
+            >
+              I agree to the{" "}
+              <Text style={styles.text_blue}>Terms of Conditions</Text> and{" "}
+              <Text style={styles.text_blue}>Privacy Policy</Text>
+            </CheckBox>
 
-          <Button style={styles.button} size="small">
-            Signup
-          </Button>
+            <Button style={styles.button} size="medium">
+              Let's Go!
+            </Button>
+          </Layout>
         </Layout>
       </ScrollView>
     </SafeAreaView>
@@ -79,24 +114,38 @@ export const SignupScreen = () => {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: -25,
     display: "flex",
     flexDirection: "column",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+  },
+  hero: {
+    backgroundColor: "#301A4B",
+    height: 175,
   },
   input: {
-    paddingLeft: 10,
-    paddingRight: 10,
+    paddingLeft: 15,
+    paddingRight: 15,
     marginTop: 10,
     marginBottom: 20,
   },
 
   button: {
-    marginBottom: 10,
-    marginLeft: 10,
-    width: 150,
+    marginBottom: 15,
+    marginLeft: 15,
+    marginRight: 15,
+    backgroundColor: "#3F295A",
+    borderColor: "transparent",
   },
   checkBox: {
-    marginLeft: 10,
+    marginLeft: 15,
     marginBottom: 20,
+  },
+  text_blue: {
+    color: "#0B2F8D",
+    fontSize: 13,
+    fontWeight: "600",
   },
 });
 
